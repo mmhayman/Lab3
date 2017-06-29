eeaa198571bffd8a6c44201e450be8d828a0a4fd
mmhayman <mmhayman@svsu.edu>
2017-06-28 22:32:18 -0400

    added comments

--- src/PowersLab3.java
+++ src/PowersLab3.java
@@ -17,24 +17,24 @@ public class PowersLab3 {
             String table = "Number" + "     " + "Squared" + "    " + "Cubed";  // creating the table
 
             String table1 = "======" + "     " + "=======" + "    " + "======"; // creating the table
-            System.out.println(table);
-            System.out.println(table1);
+            System.out.println(table); // printing the table so it it set up correctly
+            System.out.println(table1); // printing second half so it looks pretty
 
-            int numberSquared = 0;
-            int numberCubed = 0;
+            int numberSquared = 0; // initializing
+            int numberCubed = 0; // intializing
 
-            String out = "";
-            for (int i = 1; i <= userNum; i++) {
+            String out = ""; // initializing the string
+            for (int i = 1; i <= userNum; i++) { // for loop
 
-                numberSquared = (int) Math.pow(i, 2);
-                numberCubed = (int) Math.pow(i, 3);
+                numberSquared = (int) Math.pow(i, 2); // squaring user input
+                numberCubed = (int) Math.pow(i, 3); // cubing user input
 
-                out = ( i + "            " +  numberSquared  + "           " + numberCubed );
-                System.out.println(out);
+                out = ( i + "            " +  numberSquared  + "           " + numberCubed ); // creating string line
+                System.out.println(out); // outputting string
             }
 
 
-            System.out.println("Do you want to continue? (y/n)");
+            System.out.println("Do you want to continue? (y/n)"); // asking if user wants to continue
             choice = scan.next();
             System.out.println();
 
